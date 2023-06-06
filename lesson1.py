bd7bee5e159ac398853b0175ab9daff6a0577d9d
AG-Force <randa712807@icloud.com>
2023-06-06 13:10:01 +0200

    syntax and first-App

---
+++ lesson1.py
@@ -0,0 +1,13 @@
+print ("I love python")
+######################################
+print ("I love python"); print ("I love python to")
+
+if True:
+                                               print ("I love python")
+if True:
+                print ("I love python to")
+                print ("I love python to ")
+if True:
+        if True:
+                                                             print ("I love python to")
+                                                             print ("I love python to ")

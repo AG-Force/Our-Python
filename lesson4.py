922ca4d95c444688d97170bab5a1557ea445e2b4
AG-Force <randa712807@icloud.com>
2023-06-07 10:41:48 +0200

    Some data types

---
+++ lesson4.py
@@ -0,0 +1,21 @@
+#---------------------------------------------------------------#
+#type() um den Datentypen zu ermitteln
+#All data in python are objects
+#---------------------------------------------------------------#
+print(type(10)) # int
+print(type(10.3)) # float
+print(type(-10.3)) # float
+print(type(0)) # int
+print(type("Hallo")) # str
+print(type([1,2,3,4,5,6,7,8,9,])) #list
+print(type((1,2,3,4,5,6))) #tuple
+print(type({1:2,"Hallo":3})) #dictionary
+print(2==2)# True
+print(2!=2)# False
+
+
+
+
+
+
+

cc5b244536d0ef0d412c04850de4ef0adb589830
AG-Force <randa712807@icloud.com>
2023-06-10 14:50:43 +0200

    variables

---
+++ lesson5.py
@@ -0,0 +1,9 @@
+# "Variables should begin with A-Z or a-z. It is not allowed to start with numbers or special characters."
+#"يجب أن تبدأ المتغيرات بـ A-Z أو a-z. لا يُسمح ببدء المتغيرات بالأرقام أو الأحرف الخاصة."
+#"Variablen sollten mit A-Z oder a-z beginnen. Es ist nicht erlaubt, mit Zahlen oder Sonderzeichen zu beginnen."
+
+
+myvariable = "my Value"
+print(myvariable)
+my_Name = "Gassan Abdalhamid"
+print(my_Name)

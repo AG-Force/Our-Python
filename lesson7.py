6da397219f800be769726af63c4c13612b448d5c
AG-Force <randa712807@icloud.com>
2023-06-10 17:33:38 +0200

    Escape sequence characters

---
+++ lesson7.py
@@ -0,0 +1,34 @@
+ #Will Remove o
+print("Hallo\bWelt")
+
+
+#Escape new line + Back slach
+print("Hallo \
+Welt \
+wie geht es dir")
+      
+
+#Escape  Back slach
+print("Hallo Welt\"")
+print("Hallo Welt\\")
+
+
+#Escape single Quote
+print('Hallo Welt \'Test1\' ')
+
+
+#Escape double Quote
+print("Hallo Welt \"Test2\" ")
+
+# Line feed
+print("Hallo\nWelt") 
+
+#carriage return
+print("123456\rAbcd") 
+
+# Horizontal Tab
+print("Hallo\tpython")
+# Character hex value
+print("\x4F\x73")
+
+

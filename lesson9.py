44ca143fc503f9de1db97b58dd1645d0146d60e0
AG-Force <randa712807@icloud.com>
2023-06-15 17:51:03 +0200

    string variable

---
+++ lesson9.py
@@ -0,0 +1,56 @@
+myString1 = 'Hallo1'  # Define a string variable using single quotes
+myString2 = "Hallo2"  # Define a string variable using double quotes
+print(myString1)  # Print the value of myString1
+print(myString2)  # Print the value of myString2
+#################################
+myString3 = 'this is a test "hello"'  # Define a string with double quotes inside
+print(myString3)  # Print the value of myString3
+#################################
+myString4 = "this is a test 'hello'"  # Define a string with single quotes inside
+print(myString4)  # Print the value of myString4
+#################################
+myString5 = """
+so we can write text in multiple lines 
+test
+test2
+test
+test4
+"""  # Define a multiline string using triple double quotes
+print(myString5)  # Print the value of myString5
+#################################
+myString6 = '''
+so we can write text in multiple lines 
+test
+test2
+test
+test4
+'''  # Define a multiline string using triple single quotes
+print(myString6)  # Print the value of myString6
+#################################
+myString7 = """
+so we can write text in multiple lines 
+test
+'test2'
+"test"
+test4
+"""  # Define a multiline string with mixed quotes
+print(myString7)  # Print the value of myString7
+#################################
+myString8 = '''
+so we can write text in multiple lines 
+"test"
+'test2'
+"test"
+test4
+'''  # Define a multiline string with mixed quotes
+print(myString8)  # Print the value of myString8
+#################################
+myString9 = '''
+look out 
+so we can write\\\ text in multiple lines 
+"test1"
+'test2' \
+"test3"
+test4
+'''  # Define a multiline string with backslashes and mixed quotes
+print(myString9)  # Print the value of myString9

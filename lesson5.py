a42aeabbcd1db4fa12bb18bf2e3e9e2059f113eb
Gassan Abdalhamid <gassan@localhost.localdomain>
2023-06-23 11:41:12 +0200

    geändert

--- lesson5.py
+++ lesson5.py
@@ -1,4 +1,5 @@
 # "Variables should begin with A-Z or a-z. It is not allowed to start with numbers or special characters."
+
 #"يجب أن تبدأ المتغيرات بـ A-Z أو a-z. لا يُسمح ببدء المتغيرات بالأرقام أو الأحرف الخاصة."
 #"Variablen sollten mit A-Z oder a-z beginnen. Es ist nicht erlaubt, mit Zahlen oder Sonderzeichen zu beginnen."
 

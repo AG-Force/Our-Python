36a81d708504b366e6350f4b3c153e33886880c8
Gassan Abdalhamid <gassan@localhost.localdomain>
2023-06-23 11:42:44 +0200

    string part three

---
+++ lesson14.py
@@ -0,0 +1,12 @@
+a = "Hallo Welt"
+print(a.replace("Hallo", "1"))
+###############################
+b = "a a a a a a a a a "
+print(b.replace("a", "1" , 1))
+###############################
+c = ["Gassan", "Ahmed", "Zakaria"]
+print("-".join(c))
+print(":)".join(c))
+print(type(":)".join(c)))
+
+

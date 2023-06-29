6284c911308fba9a9932898ce43b4de3b85dc131
Gassan Abdalhamid <gassan@localhost.localdomain>
2023-06-29 08:39:50 +0200

    Arithmetic Operators

---
+++ lesson17.py
@@ -0,0 +1,19 @@
+#####lesson19 according youtube. Numbers
+print(type(1))
+print(type(10))
+print(type(100))
+print(type(-1))
+print(type(10.500))
+
+##########################coplex numbers#####################################################
+
+mycomplexNumber = 5+6j
+
+print(type(mycomplexNumber))
+
+print("Real Part is: {}".format(mycomplexNumber.real))
+print("imaginary Part is: {}".format(mycomplexNumber.imag))
+
+print(float(100))
+print(int(5.00030303))
+print(complex(100))
---
+++ lesson18.py
@@ -0,0 +1,17 @@
+###############################lesson20 according youtube arithmetic operators###############################
+
+
+print(10+20)
+print(4-5)
+print(2*3)
+print(6/2)
+print(9%2)
+print(4+3*4)
+print((4+3)*4)
+print((50+50)/2)
+#exponent
+print(5**5)
+print(5**2)
+#floor division
+print(100//20)
+print(110 // 20)
